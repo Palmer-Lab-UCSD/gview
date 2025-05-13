1f81b1fcf86879c9acd786a74afb0e31deaa386e
Robert Vogel <12845765+robert-vogel@users.noreply.github.com>
2025-05-13 13:35:45 -0400

    simple update

--- web/src/types/lz.d.ts
+++ web/src/types/lz.d.ts
@@ -1,35 +1,36 @@
-declare class BaseAdapter {}
-
-declare class BaseLZAdapter {
-    constructor(config: object)
-    _url: string
-    _getURL(request_options: any): string
-    _normalizeResponse(response_text: string,
-        options: any): JSON
-}
-
-declare class RegistryBase {
-    get(name: string): typeof BaseLZAdapter
-}
-declare class ClassRegistry extends RegistryBase {}
-
-declare interface Plot {}
-declare class MyDataSources {
-    constructor(registry?: RegistryBase);
-    add(namesapce: string,
-        adapter: BaseLZAdapter,
-        override?: boolean): MyDataSources;
-}
 
 declare namespace LocusZoom {
-    export const DataSources: typeof MyDataSources
-    export const Adapters: ClassRegistry
-    export function populate(selector: string, datasource: DataSources, layout: object): Plot
-    export type Panel = {
-        id:             null | string 
-        height:         
-        data_layers: Array<number> 
-    }
+    class BaseAdapter {};
+    class RegistryBase {
+        get(name: string): typeof BaseLZAdapter;
+    };
+
+    class ClassRegistry extends RegistryBase {};
+
+
+    class BaseLZAdapter {
+        constructor(config: object)
+        _url: string
+        _getURL(request_options: any): string
+        _normalizeResponse(response_text: string,
+            options: any): JSON
+    };
+
+    class DataSources {
+        constructor(registry?: RegistryBase);
+        add(namesapce: string,
+            adapter: BaseLZAdapter,
+            override?: boolean): DataSources;
+    };
+    class Adapters extends ClassRegistry {};
+
+    function populate(selector: string, datasource: DataSources, layout: object): Plot
+    type Panel = {
+        id:             null | string;
+        height:         null | number;
+        data_layers:    Array<number>;
+    };
+    interface Plot {};
 }
 
 declare type config = {
